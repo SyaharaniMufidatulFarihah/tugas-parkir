19b3592c83efccf9ee7bf631af27feb8cfc7589a
farr <97414301+SyaharaniMufidatulFarihah@users.noreply.github.com>
2022-01-10 07:45:49 +0700

    Add files via upload

---
+++ 001.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+{
+	int masuk, keluar, lama, biaya;
+	printf("Menghitung Biaya Parkir");
+	printf("\nJam Masuk: ");
+	scanf("%i",&masuk);
+	
+	printf("\nJam Keluar: ");
+	scanf("%i",&keluar);
+	if (keluar>masuk)
+		lama*keluar-masuk;
+	else 
+		lama*(12-masuk)+keluar;
+	if (lama>2)
+		biaya=2000+((lama-2)*500);
+	else
+		biaya=2000;
+	printf("\n Biaya Parkir Adalah= Rp %d\n",biaya);
+}
